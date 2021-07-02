ff0315b81e2dfa8e640d57f38d6498ad214c01e6
Lenard Frommelt <lenny09918050@googlemail.com>
2021-07-02 14:40:45 +0200

    2

--- debugmodule.js
+++ debugmodule.js
@@ -13,14 +13,14 @@
   //#############################################################################
   // console.log "debugmodule.initialize - nothing to do"
   debugmodule.modulesToDebug = {
-    unbreaker: true,
-    // cliargumentsmodule: true
-    // configmodule: true
-    // mainprocessmodule: true
-    networkinterfacemodule: true,
-    scifilesmodule: true
+    unbreaker: true
   };
 
+  // cliargumentsmodule: true
+  // configmodule: true
+  // mainprocessmodule: true
+  // networkinterfacemodule: true
+  // scifilesmodule: true
   // startupmodule: true
   module.exports = debugmodule;
 
