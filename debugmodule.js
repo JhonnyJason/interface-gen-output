bd481d7bf469a6c195a1564e130fdc3a2774dcc8
Lenard Frommelt <lenny09918050@gmail.com>
2022-04-26 19:16:15 +0200

    2

--- debugmodule.js
+++ debugmodule.js
@@ -4,17 +4,16 @@ import {
 } from "thingy-debug";
 
 //###############################################################################
-addModulesToDebug({
-  // cliargumentsmodule: true
-  // configmodule: true
-  // definitionfilemodule: true
-  filesparsermodule: true,
-  linkedmapmodule: true,
-  mainprocessmodule: true,
-  // networkinterfacemodule: true
-  pathhandlermodule: true
-});
+addModulesToDebug({});
 
+// cliargumentsmodule: true
+// configmodule: true
+// definitionfilemodule: true
+// filesparsermodule: true
+// linkedmapmodule: true
+// mainprocessmodule: true
+// networkinterfacemodule: true
+// pathhandlermodule: true
 // scifilesmodule: true
 // startupmodule: true
 // testingfilesmodule: true
