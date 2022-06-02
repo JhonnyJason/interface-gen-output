46df73b1713bf2e49b7e8a52e4271af9bf933318
Lenard Frommelt <lenny09918050@gmail.com>
2022-06-02 17:49:40 +0200

    2

--- allmodules.js
+++ allmodules.js
@@ -3,9 +3,13 @@ var Modules;
 
 import * as cliargumentsmodule from "./cliargumentsmodule.js";
 
+import * as commonstructuremodule from "./commonstructuremodule.js";
+
 import * as configmodule from "./configmodule.js";
 
 import * as debugmodule from "./debugmodule.js";
+
+import * as definitionfilemodule from "./definitionfilemodule.js";
 
 import * as filesparsermodule from "./filesparsermodule.js";
 
@@ -13,10 +17,18 @@ import * as linkedmapmodule from "./linkedmapmodule.js";
 
 import * as mainprocessmodule from "./mainprocessmodule.js";
 
+import * as networkinterfacemodule from "./networkinterfacemodule.js";
+
 import * as pathhandlermodule from "./pathhandlermodule.js";
+
+import * as scifilesmodule from "./scifilesmodule.js";
 
 import * as startupmodule from "./startupmodule.js";
 
-Modules = {cliargumentsmodule, configmodule, debugmodule, filesparsermodule, linkedmapmodule, mainprocessmodule, pathhandlermodule, startupmodule};
+import * as stateguardianmodule from "./stateguardianmodule.js";
+
+import * as testingfilesmodule from "./testingfilesmodule.js";
+
+Modules = {cliargumentsmodule, commonstructuremodule, configmodule, debugmodule, definitionfilemodule, filesparsermodule, linkedmapmodule, mainprocessmodule, networkinterfacemodule, pathhandlermodule, scifilesmodule, startupmodule, stateguardianmodule, testingfilesmodule};
 
 export default Modules;
